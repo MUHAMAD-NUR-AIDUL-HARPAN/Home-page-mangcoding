8fdfac145fbe32e116762f970659eae177847d61
Harpan <muhamadnurharpan>
2025-02-09 21:43:44 +0700

    memperbaiki tampilan header dan menambahkan icon serch

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Pastikan path ini sesuai dengan struktur proyekmu
+    safelist: [
+      "hidden",
+      "block",
+      "flex",
+      "grid",
+      "bg-red-500",
+      "text-white",
+      "p-4"
+    ], // Pastikan class ini tidak dihapus saat build
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
